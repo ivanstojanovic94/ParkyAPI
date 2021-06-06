4eeffd4124856cb6eea99f4389dd8a7e2ea4c44a
Ivan <stojanovicivan717@gmail.com>
2021-06-06 21:57:51 +0200

    Added Post method

--- ParkyAPI/Controllers/NationalParksController.cs
+++ ParkyAPI/Controllers/NationalParksController.cs
@@ -1,6 +1,8 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using ParkyAPI.Models;
+using ParkyAPI.Models.Dtos;
 using ParkyAPI.Repository.IRepository;
 using System;
 using System.Collections.Generic;
@@ -21,11 +23,52 @@ namespace ParkyAPI.Controllers
             _npRepo = npRepo;
             _mapper = mapper;
         }
+
         [HttpGet]
         public IActionResult GetNationalParks()
         {
             var objList = _npRepo.GetNationalParks();
-            return Ok(objList);
+            var objDto = new List<NationalParkDto>();
+            foreach(var obj in objList)
+            {
+                objDto.Add(_mapper.Map<NationalParkDto>(obj));
+            }
+            return Ok(objDto);
+        }
+
+        [HttpGet("{naionalParkId:int}")]
+        public IActionResult GetNationalPark(int nationalParkId)
+        {
+            var obj = _npRepo.GetNationalPark(nationalParkId);
+            if (obj == null)
+                return NotFound();
+
+            var objDto = _mapper.Map<NationalParkDto>(obj);
+            return Ok(objDto);
+        }
+
+        [HttpPost]
+        public IActionResult CreateNationalPark([FromBody] NationalParkDto nationalParkDto)
+        {
+            if (nationalParkDto == null)
+                return BadRequest(ModelState);
+
+            if (_npRepo.NationalParkExists(nationalParkDto.Name))
+            {
+                ModelState.AddModelError("", "National Park Exists!");
+                return StatusCode(404, ModelState);
+            }
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            var nationalParkObj = _mapper.Map<NationalPark>(nationalParkDto);
+
+            if (!_npRepo.CreateNationalPark(nationalParkObj))
+            {
+                ModelState.AddModelError("", $"Something went wrong when saving the record {nationalParkObj.Name}");
+                return StatusCode(500, ModelState);
+            }
+            return Ok();
         }
     }
 }
